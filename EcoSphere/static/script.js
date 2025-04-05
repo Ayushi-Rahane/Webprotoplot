747d76440781eac39dec7e10ce78fdc62a8d8cbc
Ayushi-Rahane <ayushirahane2021@gmail.com>
2025-04-05 13:38:37 +0530

    loading screenshots

--- EcoSphere/static/script.js
+++ EcoSphere/static/script.js
@@ -2,8 +2,8 @@ window.addEventListener("load", () => {
     setTimeout(() => {
       const splash = document.getElementById("splash");
       splash.style.opacity = 0;
-      setTimeout(() => splash.style.display = "none", 1);
-    }, 3);
+      setTimeout(() => splash.style.display = "none", 1000);
+    }, 3000);
   });
 
 
